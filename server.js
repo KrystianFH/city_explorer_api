32a6e3526d95e994a720b2d30d0a70a665e1285a
KrystianFH <krystianfrancuz@gmail.com>
2020-08-17 17:02:33 -0700

    added in city key

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const { json } = require('body-parser');
 
 
 //============== Global Variables ==============
@@ -12,12 +13,13 @@ const app = express();
 app.use(cors());
 
 //=============== Routes ======================
-// app.get('/location', (request, response) =>{
-//   const jsonLocationObject = require('./data/location.json');
-//   const constructedLocation = new Location(jsonLocationObject);
+app.get('/location', (request, response) =>{
+  const jsonLocationObject = require('./data/location.json');
+  const city = request.query.city;
+  const constructedLocation = new Location(jsonLocationObject, city);
 
-//   response.send(constructedLocation);
-// });
+  response.send(constructedLocation);
+});
 
 // app.get('/weather', sendWeatherData);
 
@@ -26,5 +28,16 @@ app.use(cors());
 //   const arrWeatherJson = jsonWeatherObject.
 // }
 
+//================= Other Functions ================
+function Location(jsonLocationObject, city){
+  console.log(jsonLocationObject);
+
+  this.formatted_query = jsonLocationObject[0].display_name;
+  this.latitude = jsonLocationObject[0].lat;
+  this.longitude = jsonLocationObject[0].lon;
+  this.search_query = city;
+}
+
+//===============Start the Server====================
 app.listen(PORT, () => console.log(`we are running on PORT : ${PORT}`));
 
