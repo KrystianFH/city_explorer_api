83944f23fa0d5ba0db0fe551897d1dff8f764ab8
KrystianFH <krystianfrancuz@gmail.com>
2020-08-18 16:17:38 -0700

    reconfigured routes and added in trail route and constructor

--- server.js
+++ server.js
@@ -3,38 +3,73 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-const { response } = require('express');
-const { request } = require('http');
-// const { json } = require('body-parser');
+const superagent = require('superagent');
 
 
 //============== Global Variables ==============
 
 const PORT = process.env.PORT || 3003;
+const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const TRAIL_API_KEY = process.env.TRAIL_API_KEY;
 const app = express();
 app.use(cors());
 
 //=============== Routes ======================
-app.get('/location', (request, response) =>{
-  const jsonLocationObject = require('./data/location.json');
+app.get('/location', sendLocationData);
+function sendLocationData(request, response){
   const city = request.query.city;
-  const constructedLocation = new Location(jsonLocationObject, city);
+  const urlToSearch = `https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${city}&format=json`;
 
-  response.send(constructedLocation);
-});
+  superagent.get(urlToSearch)
+    .then(whateverComesBack =>{
+      const superagentResultArray = whateverComesBack.body;
+      const constructedLocation = new Location(superagentResultArray);
+      response.send(constructedLocation);
+    })
+    .catch(error => {
+      console.log(error);
+      response.status(500).send(error.message);
+    });
+}
 
-// if(request.query.city !== 'Lynnwood'){
-//   return response.status(500).send('Use `Lynnwood`');
-// }
 
-app.get('/weather', (request, response) => {
-  const jsonWeatherObject = require('./data/weather.json');
-  const weatherArr = [];
-  jsonWeatherObject.data.forEach(forecast => {
-    weatherArr.push(new Weather(forecast));
-  });
-  response.send(weatherArr);
-});
+app.get('/weather', sendWeatherData);
+function sendWeatherData(request, response){
+  let latitude = request.query.latitude;
+  let longitude = request.query.longitude;
+  const urlToSearchForWeather = `https://api.weatherbit.io/v2.0/forecast/daily?&lat=${latitude}&lon=${longitude}&key=${WEATHER_API_KEY}`;
+
+  superagent.get(urlToSearchForWeather)
+    .then(weatherComingBack =>{
+      const weatherPass = weatherComingBack.body.data;
+      const weatherArr = weatherPass.map(index => new Weather(index));
+      response.send(weatherArr);
+    })
+    .catch(error =>{
+      console.log(error);
+      response.status(500).send(error.message);
+    });
+}
+
+app.get('/trails', sendTrailData);
+function sendTrailData(request, response){
+  let latitude = request.query.latitude;
+  let longitude = request.query.longitude;
+  const urlToSearchForTrail = `https://www.hikingproject.com/data/get-trails?lat=${latitude}&lon=${longitude}&maxDistance=10&key=${TRAIL_API_KEY}`;
+
+  superagent.get(urlToSearchForTrail)
+    .then(trailsComingBack =>{
+      const trailPass = trailsComingBack.body.data;
+      const trailArr = trailPass.map(index => new Trail(index));
+      response.send(trailArr);
+    })
+    .catch(error =>{
+      console.log(error);
+      response.status(500).send(error.message);
+    });
+}
+
 
 //================= Other Functions ================
 function Location(jsonLocationObject, city){
@@ -51,6 +86,11 @@ function Weather(jsonWeatherObject){
   this.time = jsonWeatherObject.valid_date;
 }
 
+function Trail(jsonTrailObject){
+  this.name = jsonTrailObject[0].name;
+  this.length = jsonTrailObject[0].length;
+  this.summary = jsonTrailObject[0].summary;
+}
+
 //===============Start the Server====================
 app.listen(PORT, () => console.log(`we are running on PORT : ${PORT}`));
-
